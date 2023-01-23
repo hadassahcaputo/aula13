ad1d723c5c97f6cbf002e8de4ff9b3bc8a039c77
Hadassah Caputo <hadassahpaz13@hotmail.com>
2023-01-23 10:45:15 -0300

    Initial commit

---
+++ ambiente.js
@@ -0,0 +1,18 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c ++ //c + 1
+} while (c <= 7)
+
+
+/*
+var c = 1
+while (c <= 7) {
+    console.log(`Passo ${c}`)
+    c ++ //c + 1
+}
+*/
+
+
+
+//console.log('Tudo bem?')
